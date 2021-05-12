83a056ce0220541ca280eed6d8f65ecc395a04b6
LisbethWL <contact@spacegap.dk>
2021-05-12 09:20:45 +0200

    first commit learning bot

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<header>
+  <nav class="nav nav-bar">
+
+      <li class="nav-item">
+        <router-link :to="{ name: 'Home' }" exact>
+          <img class="logo" src="./assets/build-a-bot-logo.png" />
+          Build-a-Bot
+        </router-link>
+      </li>
+
+      <router-link :to="{ name: 'Build' }" exact>Build</router-link>
+
+  </nav>
+</header>
+  <main>
+
+    <router-view />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style>
+  body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+</style>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 100%;
+  margin: 0px 20px 20px 0px;
+}
+ul {
+  padding: 13px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+  color: white;
+}
+.nav-bar a {
+  font-weight: bold;
+  color: white;
+  text-decoration: none;
+  padding: 10px;
+}
+.nav-bar a:hover {
+  color: pink;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
